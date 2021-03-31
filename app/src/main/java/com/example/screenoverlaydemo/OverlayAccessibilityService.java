fa915b19e5be966fd4290c60feafbdddd2fd57da
Sébastian Pilon <sebastian@iptoki.com>
2021-03-31 09:07:00 -0400

    Log other things

--- app/src/main/java/com/example/screenoverlaydemo/OverlayAccessibilityService.java
+++ app/src/main/java/com/example/screenoverlaydemo/OverlayAccessibilityService.java
@@ -44,31 +44,28 @@ public class OverlayAccessibilityService extends AccessibilityService {
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+
+        if(accessibilityEvent.getEventType() == AccessibilityEvent.TYPE_GESTURE_DETECTION_START) {
+            Log.d(TAG, "a gesture");
+        }
+
         AccessibilityNodeInfo source = accessibilityEvent.getSource();
         if (source == null) {
             return;
         }
         Log.d(TAG, "Infos onAccessibilityEvent --- ");
         Log.d(TAG, "Event : " + accessibilityEvent);
+        Log.d(TAG, "Event Action : " + accessibilityEvent.getAction());
+        Log.d(TAG, "Event Type : " + getEventTypeString(accessibilityEvent.getEventType()));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            Log.d(TAG, "Event Window Changes : " + accessibilityEvent.getWindowChanges());
+        }
+        Log.d(TAG, "Event Package Name : " + accessibilityEvent.getPackageName());
+        Log.d(TAG, "Event Content Description : " + accessibilityEvent.getContentDescription());
+        Log.d(TAG, "Event Movement granularity : " + accessibilityEvent.getMovementGranularity());
+        Log.d(TAG, "Event Time : " + accessibilityEvent.getEventTime());
+        Log.d(TAG, "Event Content change types : " + accessibilityEvent.getContentChangeTypes());
         Log.d(TAG, "Description : " + source.getContentDescription());
-        Log.d(TAG, "Actions : " + source.getActionList());
-        Log.d(TAG, "Package name : " + source.getPackageName());
-        Log.d(TAG, "Window : " + source.getWindow());
-        Log.d(TAG, "Collection Infos : " + source.getCollectionInfo());
-        Log.d(TAG, "ViewId Name : " + source.getViewIdResourceName());
-        Log.d(TAG, "Extra Data : " + source.getAvailableExtraData());
-        Log.d(TAG, "Class Name : " + source.getClassName());
-        Log.d(TAG, "Extra : " + source.getExtras());
-        Log.d(TAG, "Live region : " + source.getLiveRegion());
-        Log.d(TAG, "Input type : " + source.getInputType());
-        Log.d(TAG, "Text selection start : " + source.getTextSelectionStart());
-        Log.d(TAG, "Text selection end : " + source.getTextSelectionEnd());
-        Log.d(TAG, "Movement granularity : " + source.getMovementGranularities());
-        Log.d(TAG, "Traversal before : " + source.getTraversalBefore());
-        Log.d(TAG, "Traversal after : " + source.getTraversalAfter());
-        Log.d(TAG, "Collection info : " + source.getCollectionInfo());
-        Log.d(TAG, "Collection Item info : " + source.getCollectionItemInfo());
-//        Log.d(TAG, "Screen click " + source.get + " - Screen -> W:" + screenWidth + " H:" + screenHeight);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             Log.d(TAG, "Extra rendering info : " + source.getExtraRenderingInfo());
@@ -85,17 +82,70 @@ public class OverlayAccessibilityService extends AccessibilityService {
     @Override
     public boolean onGesture(@NonNull AccessibilityGestureEvent gestureEvent) {
         Log.d(TAG, "onGestureEvent: event ");
-        return super.onGesture(gestureEvent);
+//        return super.onGesture(gestureEvent);
+        return false;
     }
 
     @Override
     protected boolean onKeyEvent(KeyEvent event) {
         Log.d(TAG, "onKeyEvent: event ");
-        return super.onKeyEvent(event);
+//        return super.onKeyEvent(event);
+        return false;
     }
 
     @Override
     public void onInterrupt() {
+    }
 
+    private String getEventTypeString(int eventType) {
+        switch (eventType) {
+            case AccessibilityEvent.TYPE_ANNOUNCEMENT:
+                return "TYPE_ANNOUNCEMENT";
+            case AccessibilityEvent.TYPE_GESTURE_DETECTION_END:
+                return "TYPE_GESTURE_DETECTION_END";
+            case AccessibilityEvent.TYPE_GESTURE_DETECTION_START:
+                return "TYPE_GESTURE_DETECTION_START";
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                return "TYPE_NOTIFICATION_STATE_CHANGED";
+            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END:
+                return "TYPE_TOUCH_EXPLORATION_GESTURE_END";
+            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:
+                return "TYPE_TOUCH_EXPLORATION_GESTURE_START";
+            case AccessibilityEvent.TYPE_TOUCH_INTERACTION_END:
+                return "TYPE_TOUCH_INTERACTION_END";
+            case AccessibilityEvent.TYPE_TOUCH_INTERACTION_START:
+                return "TYPE_TOUCH_INTERACTION_START";
+            case AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUSED:
+                return "TYPE_VIEW_ACCESSIBILITY_FOCUSED";
+            case AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUS_CLEARED:
+                return "TYPE_VIEW_ACCESSIBILITY_FOCUS_CLEARED";
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+                return "TYPE_VIEW_CLICKED";
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+                return "TYPE_VIEW_FOCUSED";
+            case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:
+                return "TYPE_VIEW_HOVER_ENTER";
+            case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT:
+                return "TYPE_VIEW_HOVER_EXIT";
+            case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
+                return "TYPE_VIEW_LONG_CLICKED";
+            case AccessibilityEvent.TYPE_VIEW_SCROLLED:
+                return "TYPE_VIEW_SCROLLED";
+            case AccessibilityEvent.TYPE_VIEW_SELECTED:
+                return "TYPE_VIEW_SELECTED";
+            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
+                return "TYPE_VIEW_TEXT_CHANGED";
+            case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED:
+                return "TYPE_VIEW_TEXT_SELECTION_CHANGED";
+            case AccessibilityEvent.TYPE_VIEW_TEXT_TRAVERSED_AT_MOVEMENT_GRANULARITY:
+                return "TYPE_VIEW_TEXT_TRAVERSED_AT_MOVEMENT_GRANULARITY";
+            case AccessibilityEvent.TYPE_WINDOWS_CHANGED:
+                return "TYPE_WINDOWS_CHANGED";
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+                return "TYPE_WINDOW_CONTENT_CHANGED";
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                return "TYPE_WINDOW_STATE_CHANGED";
+        }
+        return String.format("unknown (%d)", eventType);
     }
 }
